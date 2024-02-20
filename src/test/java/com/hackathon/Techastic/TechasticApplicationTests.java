e4469dccc280731a453093139e43d9795fcb2b53
Komala <105430476+InterFITechastic@users.noreply.github.com>
2024-02-20 16:31:13 -0500

    Update TechasticApplicationTests.java

--- src/test/java/com/hackathon/Techastic/TechasticApplicationTests.java
+++ src/test/java/com/hackathon/Techastic/TechasticApplicationTests.java
@@ -3,7 +3,7 @@ package com.hackathon.Techastic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes={TechasticApplication.class})
 class TechasticApplicationTests {
 
 	@Test
