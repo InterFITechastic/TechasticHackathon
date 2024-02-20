20e644bb26e99a8c122ab36ff13bef59e52d3d1c
Komala <105430476+InterFITechastic@users.noreply.github.com>
2024-02-20 16:22:18 -0500

    Update FindNationalityUtil.java

--- src/main/java/com/hackathon/techastic/util/FindNationalityUtil.java
+++ src/main/java/com/hackathon/techastic/util/FindNationalityUtil.java
@@ -23,7 +23,7 @@ public class FindNationalityUtil {
 
             URL url = new URL("https://ono.4b.rs/v1/nat?key=0ab808d14ef8450da38af5f7f32c59e0&fn="
                     + URLEncoder.encode(name, StandardCharsets.UTF_8)
-                    + "&sn=" + URLEncoder.encode("Sanguino", StandardCharsets.UTF_8));
+                    + URLEncoder.encode("Sanguino", StandardCharsets.UTF_8));
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
             if (conn.getResponseCode() != 200) {
